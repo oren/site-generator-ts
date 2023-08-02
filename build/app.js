02cec642b771326779a31f21fdde638c49de49e6
Oren Golan <orengolan@gmail.com>
2023-08-01 22:59:31 -0700

    misc

--- build/app.js
+++ build/app.js
@@ -1,2 +1,4 @@
 "use strict";
+// convert README.md to index.html
+// call the function on each sub-folder
 console.log('Node.js TypeScript');
--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,4 @@
+// convert README.md to index.html
+// call the function on each sub-folder
 console.log('Node.js TypeScript');
+
